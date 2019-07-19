30282c19e88414966926519bbe8eda76518f2b56
James Baker <baker.james.jb@gmail.com>
2019-07-19 15:25:28 +0100

    migration overhaul

--- scripts/cys_loop_test_query.py
+++ scripts/cys_loop_test_query.py
@@ -17,7 +17,7 @@ import Bio
 from Bio import SeqIO
 from Bio import SwissProt
 from Bio.SeqUtils.ProtParam import ProteinAnalysis
-# env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install psycopg2
+# env LDFLAGS="-I/usr/local/opt/openssl/include -L/usr/local/opt/openssl/lib" pip install_vars psycopg2
 from django.conf import settings
 from django.db import models
 from tmh_db.models import Database_Metadata, Subcellular_location, Uniref, Go, Structure, Structural_residue, Funfam_residue, Funfamstatus, Protein, Residue, Tmh, Tmh_deltag, Tmh_hydrophobicity, Tmh_residue, Tmh_tmsoc, Variant, Keyword, Binding_residue
@@ -29,7 +29,7 @@ from scripts.populate_general_functions import *
 
 file = open("lista_uniprots.txt", "r")
 
-all=0
+all_vars=0
 tmh=0
 non_tmh=0
 flanks=0
@@ -40,17 +40,16 @@ for aline in file:
     print(aline)
     #print(Variant.objects.filter(residue__protein__uniprot_id=aline).values_list("residue__sequence_position"))
     print(Variant.objects.filter(variant_source="ClinVar", disease_status='d', residue__protein__uniprot_id=aline).values_list( "residue__protein__uniprot_id", "residue__sequence_position"))
-
-    all_id_vars = Variant.objects.filter(variant_source="ClinVar", disease_status='d', residue__protein__uniprot_id=aline).count()
+    all_vars_id_vars = Variant.objects.filter(variant_source="ClinVar", disease_status='d', residue__protein__uniprot_id=aline).count()
     tmh_id_vars = Variant.objects.filter(variant_source="ClinVar", disease_status='d', residue__protein__uniprot_id=aline, residue__tmh_residue__feature_location="TMH").count()
     flank_id_vars = Variant.objects.filter(variant_source="ClinVar", disease_status='d', residue__protein__uniprot_id=aline, residue__tmh_residue=True).exclude(residue__tmh_residue__feature_location="TMH").count()
     non_tmh_id_vars = Variant.objects.filter(variant_source="ClinVar", disease_status='d', residue__protein__uniprot_id=aline, residue__tmh_residue=None).count()
 
-    print(all_id_vars, tmh_id_vars, flank_id_vars, non_tmh_id_vars)
-    all = all + all_id_vars
+    print(all_vars_id_vars, tmh_id_vars, flank_id_vars, non_tmh_id_vars)
+    all_vars = all_vars + all_vars_id_vars
     tmh = tmh + tmh_id_vars
     flanks = flanks + flank_id_vars
     non_tmh = non_tmh + non_tmh_id_vars
 
 print("Total Total, TMH, Flanks, Non-TMH")
-print(all, tmh, flanks, non_tmh)
+print(all_vars, tmh, flanks, non_tmh)
