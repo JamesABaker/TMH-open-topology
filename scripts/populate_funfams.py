112296be9d4767b54dfeb98cc66a77249f6ac80f
JamesABaker <baker.james.jb@gmail.com>
2020-06-09 11:45:00 +0100

    works on #65

--- scripts/populate_funfams.py
+++ scripts/populate_funfams.py
@@ -30,80 +30,94 @@ today = date.today()
 todaysdate = today.strftime("%d_%m_%Y")
 
 
+def json_decode_check(funfam_file):
+	try:
+		with open(funfam_file, "r") as json_file:
+			funfam_json = json.load(json_file)
+		return(True)
+	except(JSONDecodeError):
+		return(False) 
+
+
 def uniprot_to_funfams(a_query):
-    '''
-    Downloads the funfams associated with a uniprot id.
-    It accepts a uniprot id.
-    It returns a list of tuples for the superfamily and funfam id.
-    '''
-    funfam_url = f"http://www.cathdb.info/version/v4_2_0/api/rest/uniprot_to_funfam/{a_query}?content-type=application/json"
-    funfam_file = f'scripts/external_datasets/funfam_bin/json/{a_query}.json'
-    if check_local_file(funfam_file)==False:
-        download(funfam_url, funfam_file, pause=pause_time)
-    with open(funfam_file, "r") as json_file:
-        funfam_json = json.load(json_file)
-    funfams = []
-    for i in funfam_json["data"]:
-        funfams.append((i["superfamily_id"], i["funfam_number"]))
-    return(funfams)
+	'''
+	Downloads the funfams associated with a uniprot id.
+	It accepts a uniprot id.
+	It returns a list of tuples for the superfamily and funfam id.
+	'''
+	funfam_url = f"http://www.cathdb.info/version/v4_2_0/api/rest/uniprot_to_funfam/{a_query}?content-type=application/json"
+	funfam_file = f'scripts/external_datasets/funfam_bin/json/{a_query}.json'
+	json_check=False
+	while json_check==False: 
+		if check_local_file(funfam_file)==False:
+			download(funfam_url, funfam_file, pause=pause_time)
+		if json_decode_check(funfam_file)==False:
+			download(funfam_url, funfam_file, pause=pause_time)
+		elif json_decode_check(funfam_file)==True:
+			json_check=True
+	with open(funfam_file, "r") as json_file:
+		funfam_json = json.load(json_file)
+		funfams = []
+		for i in funfam_json["data"]:
+			funfams.append((i["superfamily_id"], i["funfam_number"]))
+		return(funfams)
 
 
 def funfam_to_stockholm(uniprot_id, superfamily_number, funfam_number):
-    '''
-    Takes the funfams and superfamilies and returns the stockholm alignment.
-    '''
-    #protein = Protein.objects.get(uniprot_id=a_query)
-    stockholm_url = f"http://www.cathdb.info/version/v4_2_0/superfamily/{superfamily_number}/funfam/{funfam_number}/files/stockholm"
-    stockholm_file = f'scripts/external_datasets/funfam_bin/stockholm/{superfamily_number}/{funfam_number}.sth'
-    cath_superfamily_folder=f'scripts/external_datasets/funfam_bin/stockholm/{superfamily_number}'
-    if check_local_file(stockholm_file)==False:
-        if not os.path.exists(cath_superfamily_folder):
-            os.makedirs(cath_superfamily_folder)
-        download(stockholm_url, stockholm_file, pause=pause_time)
-    return(stockholm_file)
+		'''
+		Takes the funfams and superfamilies and returns the stockholm alignment.
+		'''
+		#protein = Protein.objects.get(uniprot_id=a_query)
+		stockholm_url = f"http://www.cathdb.info/version/v4_2_0/superfamily/{superfamily_number}/funfam/{funfam_number}/files/stockholm"
+		stockholm_file = f'scripts/external_datasets/funfam_bin/stockholm/{superfamily_number}/{funfam_number}.sth'
+		cath_superfamily_folder=f'scripts/external_datasets/funfam_bin/stockholm/{superfamily_number}'
+		if check_local_file(stockholm_file)==False:
+			if not os.path.exists(cath_superfamily_folder):
+				os.makedirs(cath_superfamily_folder)
+				download(stockholm_url, stockholm_file, pause=pause_time)
+		return(stockholm_file)
 
 
 def stockholm_to_database(a_query, stockholm_file_location):
-    '''
-    Parses a stockholm alignment and links residues and proteins to the funfam
-    id in the VarTMH database.
-    It adds the protein to the funfam, and the residue to the funfam_residue.
-    '''
-    print(stockholm_file_location)
-    #align = AlignIO.read(stockholm_file_location, "stockholm")
-    #for record in align:
-    #    print(str(record.id))
-    #    if str(record.id) in a_query:
-    #        #print(f"{record.id}, {len(record)}")
-    #        print(record.letter_annotations["scorecons_70"])
-    #    pass
-    return()
+		'''
+		Parses a stockholm alignment and links residues and proteins to the funfam
+		id in the VarTMH database.
+		It adds the protein to the funfam, and the residue to the funfam_residue.
+		'''
+		print(stockholm_file_location)
+		#align = AlignIO.read(stockholm_file_location, "stockholm")
+		#for record in align:
+		#	 print(str(record.id))
+		#	 if str(record.id) in a_query:
+		#			 #print(f"{record.id}, {len(record)}")
+		#			 print(record.letter_annotations["scorecons_70"])
+		#	 pass
+		return()
 
 
 def run():
-    '''
-    This is what django runs.
-    This is effectively the canonical script, even though django forces
-    it to be in a function.
-    '''
+		'''
+		This is what django runs.
+		This is effectively the canonical script, even though django forces
+		it to be in a function.
+		'''
 
-    ### Canonical script starts here ###
+		### Canonical script starts here ###
 
-    input_query = input_query_get()
+		input_query = input_query_get()
 
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tmh_database.settings')
+		os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tmh_database.settings')
 
-    ### Download uniprot files ###
-    inputs = input_query_process(input_query)
-    input_queries = inputs[0]
-    input_query_set = inputs[1]
+		### Download uniprot files ###
+		inputs = input_query_process(input_query)
+		input_queries = inputs[0]
+		input_query_set = inputs[1]
 
-    for a_query in input_query:
-        funfams_superfamilies = uniprot_to_funfams(str(clean_query(a_query)))
-        if len(funfams_superfamilies) > 0:
-            for a_funfam in funfams_superfamilies:
-                superfamily_id = a_funfam[0]
-                funfam_id = a_funfam[1]
-                stockholm_file = funfam_to_stockholm(
-                    a_query, superfamily_id, funfam_id)
-                stockholm_to_database(a_query, stockholm_file)
+		for a_query in input_query:
+			funfams_superfamilies = uniprot_to_funfams(str(clean_query(a_query)))
+			if len(funfams_superfamilies) > 0:
+				for a_funfam in funfams_superfamilies:
+					superfamily_id = a_funfam[0]
+					funfam_id = a_funfam[1]
+					stockholm_file = funfam_to_stockholm(a_query, superfamily_id, funfam_id)
+					stockholm_to_database(a_query, stockholm_file)
